58ac4bfee19b76e7dc820fe800e684d5ede987d9
Alex Daniel <you@example.comalexdanieltb220@gmail.com>
2025-01-04 20:12:59 -0500

    Funcion ultimo digito

--- src/main/java/com/mycompany/taller2adminpruebas/Taller2adminpruebas.java
+++ src/main/java/com/mycompany/taller2adminpruebas/Taller2adminpruebas.java
@@ -37,40 +37,7 @@ public class Taller2adminpruebas {
                                 if (edad >= 18 && cedula.length() == 10) {
                                     ced = cedula.charAt(9);
                                     cedn = Integer.parseInt(String.valueOf(ced));
-                                    //System.out.println("Ultimo digito es: "+ced);
-                                    switch (cedn) {
-                                        case 0:
-                                            fecha = "Lunes 02 de Diciembre del 2024";
-                                            break;
-                                        case 1:
-                                            fecha = "Martes 03 de Diciembre del 2024";
-                                            break;
-                                        case 2:
-                                            fecha = "Miércoles 04 de Diciembre del 2024";
-                                            break;
-                                        case 3:
-                                            fecha = "Jueves 05 de Diciembre del 2024";
-                                            break;
-                                        case 4:
-                                            fecha = "Viernes 06 de Diciembre del 2024";
-                                            break;
-                                        case 5:
-                                            fecha = "Sábado 07 de Diciembre del 2024";
-                                            break;
-                                        case 6:
-                                            fecha = "Lunes 09 de Diciembre del 2024";
-                                            break;
-                                        case 7:
-                                            fecha = "Martes 10 de Diciembre del 2024";
-                                            break;
-                                        case 8:
-                                            fecha = "Miércoles 11 de Diciembre del 2024";
-                                            break;
-                                        case 9:
-                                            fecha = "Jueves 12 de Diciembre del 2024";
-                                            break;
-                                    }
-                                    JOptionPane.showMessageDialog(null, "Estimado " + nombre + " " + apellido + "\nRendirás la prueba el día " + fecha);
+                                    JOptionPane.showMessageDialog(null, "Estimado " + nombre + " " + apellido + "\nRendirás la prueba el día " + ultdigced(cedn));
                                 } else if (edad < 18) {
                                     JOptionPane.showMessageDialog(null, "Debes se mayor de edad para rendir el exámen");
                                 } else if (cedula.length() != 10) {
@@ -90,10 +57,10 @@ public class Taller2adminpruebas {
                     } while (opcion2 != 3);
                     break;
                 case 2:
-                    do{
-                       opcion2 = Integer.parseInt(JOptionPane.showInputDialog("Menú Profesores\n\nElige una opción:\n1. Ingreso de Información de Estudiante\n2. Promedio Final\n3. Regresar"));
-                       switch (opcion2){
-                           case 1:
+                    do {
+                        opcion2 = Integer.parseInt(JOptionPane.showInputDialog("Menú Profesores\n\nElige una opción:\n1. Ingreso de Información de Estudiante\n2. Promedio Final\n3. Regresar"));
+                        switch (opcion2) {
+                            case 1:
                                 nombre = JOptionPane.showInputDialog("SOLICITUD DE PRUEBA\n\nIngresa tu nombre:");
                                 apellido = JOptionPane.showInputDialog("Ingresa tu apellido:");
                                 edad = Integer.parseInt(JOptionPane.showInputDialog("Ingresa tu edad:"));
@@ -106,19 +73,19 @@ public class Taller2adminpruebas {
                                 }else if (cedula.length() != 10){
                                     
                                 }
-                                */
+                                 */
                                 JOptionPane.showMessageDialog(null, "El estudiante ha sido registrado");
                                 break;
-                           case 2:
+                            case 2:
                                 JOptionPane.showMessageDialog(null, "PROMEDIO FINAL");
                                 break;
-                           case 3:
+                            case 3:
                                 break;
-                           default:
+                            default:
                                 JOptionPane.showMessageDialog(null, "La opción ingresada no es válida");
                                 break;
-                       }
-                    }while(opcion2 != 3);
+                        }
+                    } while (opcion2 != 3);
                     break;
                 case 3:
                     break;
@@ -127,6 +94,43 @@ public class Taller2adminpruebas {
                     break;
             }
         } while (opcion1 != 3);
-        
+
+    }
+
+    public static String ultdigced(int n) {
+        String fecha = "";
+        switch (n) {
+            case 0:
+                fecha = "Lunes 02 de Diciembre del 2024";
+                break;
+            case 1:
+                fecha = "Martes 03 de Diciembre del 2024";
+                break;
+            case 2:
+                fecha = "Miércoles 04 de Diciembre del 2024";
+                break;
+            case 3:
+                fecha = "Jueves 05 de Diciembre del 2024";
+                break;
+            case 4:
+                fecha = "Viernes 06 de Diciembre del 2024";
+                break;
+            case 5:
+                fecha = "Sábado 07 de Diciembre del 2024";
+                break;
+            case 6:
+                fecha = "Lunes 09 de Diciembre del 2024";
+                break;
+            case 7:
+                fecha = "Martes 10 de Diciembre del 2024";
+                break;
+            case 8:
+                fecha = "Miércoles 11 de Diciembre del 2024";
+                break;
+            case 9:
+                fecha = "Jueves 12 de Diciembre del 2024";
+                break;
+        }
+        return fecha;
     }
 }
